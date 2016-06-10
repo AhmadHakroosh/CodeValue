21ae66705b00dab8eb7fdbc8b71edf8cf522e539
Ahmad Hakroosh <ahmad@ahmad-h.com>
2016-06-10 14:04:01 +0300

    added files and sources relevant to Lab1.2

---
+++ Lab1-2/FirstModule/FirstModule/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String FirstName = "Ahmad";
+            String LastName = "Hakroosh";
+            String Address = "Kafr Kanna, 1693000";
+            Console.WriteLine($"Hello {FirstName} {LastName}, {Address}");
+        }
+    }
+}
