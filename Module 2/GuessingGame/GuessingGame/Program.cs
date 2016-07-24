be63146511f51d8afdcc25f75b495b799363f24e
Moaid Hathot <moaid.hathot@outlook.com>
2016-07-24 13:59:55 +0300

    2.3

--- Module 2/GuessingGame/GuessingGame/Program.cs
+++ Module 2/GuessingGame/GuessingGame/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace GuessingGame
 {
+    //Nice.
     class Program
     {
         static void Main(string[] args)
@@ -15,10 +16,14 @@ namespace GuessingGame
             while (true)
             {
                 Console.WriteLine("Try to guess the number!");
+                //I have sent various mails about this bug. You should have used Next(1, 101);
                 int secret = new Random().Next(1, 100);
                 int tries = 7;
+
+                //Did you consider the use of a for loop?
                 while (tries > 0)
                 {
+                    //You are not validation the input.
                     guess = int.Parse(Console.ReadLine());
                     if (guess > secret)
                     {
